53dac908a613eb58ac223d1c1bba6eec0c948ac1
magoador <magomedov951.mail.ru>
2023-03-21 15:02:27 +0300

    Сверстал корзину

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
+import Cart from './pages/CartPage/Cart'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        
+        <Route path="/cart/:id" element={<Cart />} />
       </Routes>
     </div>
   );
