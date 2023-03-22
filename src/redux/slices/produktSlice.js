cb46203aa86ecf288047a8c4d9f3f344aa9643be
Kolizey_08 <magamedbagiev@gmail.com>
2023-03-22 16:17:45 +0300

    сделал главную страницу

---
+++ src/redux/slices/produktSlice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    produkts: []
+}
+
+export const fetchProdukt = createAsyncThunk(
+    'fetch/produkts',
+    async (data, thunkAPI) => {
+        try {
+           const produkts = await fetch('http://localhost:4000/produkt')
+           return produkts.json()
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error)
+        }
+    }
+)
+
+export const produktSlice = createSlice({
+    name: 'produkt',
+    initialState,
+    reducers: {},
+    extraReducers: (biulder) => {
+        biulder
+        .addCase(fetchProdukt.fulfilled, (state, action) => {
+            state.produkts = action.payload
+            console.log(action.payload);
+        })
+    }
+})
+
+export default produktSlice.reducer
